ab6df1ab0ed9b1b31b129365de27a6d285008994
madusug <jamesmadusha@gmail.com>
2025-06-26 19:20:54 -0400

    remove text from backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -10,12 +10,3 @@ export const backend = defineBackend({
   data
 });
 
-/**
- * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
- * @see https://docs.amplify.aws/react/build-a-backend/define-backend
- * @see https://docs.amplify.aws/react/build-a-backend/define-data
- * @see https://docs.amplify.aws/react/build-a-backend/define-auth
- * @see https://docs.amplify.aws/react/build-a-backend/define-storage
- * @see https://docs.amplify.aws/react/build-a-backend/define-functions
- * @see https://docs.amplify.aws/react/build-a-backend/define
- */
