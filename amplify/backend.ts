1d26774db6fd4b57eb6b01eb27aa326a967d6c82
madusug <jamesmadusha@gmail.com>
2025-06-26 19:08:48 -0400

    change backend

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -9,3 +9,13 @@ export const backend = defineBackend({
   auth,
   data
 });
+
+/**
+ * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
+ * @see https://docs.amplify.aws/react/build-a-backend/define-backend
+ * @see https://docs.amplify.aws/react/build-a-backend/define-data
+ * @see https://docs.amplify.aws/react/build-a-backend/define-auth
+ * @see https://docs.amplify.aws/react/build-a-backend/define-storage
+ * @see https://docs.amplify.aws/react/build-a-backend/define-functions
+ * @see https://docs.amplify.aws/react/build-a-backend/define
+ */
