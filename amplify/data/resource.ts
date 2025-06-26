65e96f789a350dc105ccbf82026caf6751de56e0
madusug <jamesmadusha@gmail.com>
2025-06-25 21:07:40 -0400

    Update resource.ts

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,15 +1,24 @@
 import { a, defineData } from '@aws-amplify/backend';
+import { renderToStream } from 'vue/server-renderer';
 
 const schema = a.schema({
   Tenant: a.model({
     name: a.string(),
     email: a.string(),
-  }),
+    bankAccountID: a.string(),
+    rentGoal: a.float(),
+    transactionHistory: a.string().array(),
+  }).authorization(allow => [allow.authenticated()]),
   Landlord: a.model({
     name: a.string(),
     property: a.string(),
-  })
-}).authorization(allow => [allow.authenticated()]);
+    bankAccountID: a.string(),
+    tenants: a.string().array(),
+    rentAmount: a.float(),
+    transactionHistory: a.string().array(),
+  }).authorization(allow => [allow.authenticated()])
+});
+
 
 export const data = defineData({
   schema
